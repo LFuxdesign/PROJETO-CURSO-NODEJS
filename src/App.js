cf3d80cf13c4162a200eb69e87e0d70207617ab0
LFuxdesign <leonardofelipe.lf173@gmail.com>
2025-03-01 22:08:28 -0400

    base

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useEffect, useState, useCallback, useRef } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  useEffect(() => {
+    const counter = setTimeout(() => {
+      try {
+        document.querySelectorAll(".entryAnimation").forEach((e) => {
+          e.style.animationDelay = "";
+        });
+      } catch (error) {
+        console.warn(
+          "erro ao limpar delay da animação de entrada dos elementos"
+        );
+      }
+    }, 2000);
+
+    return () => clearTimeout(counter);
+  }, [location]);
+
+  return (
+    <>
+        <div id="main">
+          <Routes>
+            {/* <Route exact path="/" Component={Home} />
+            <Route exact path="/app" Component={Home} />
+            <Route exact path="/nota" Component={Nota} />
+            <Route exact path="/novanota" Component={Nota} />
+            <Route exact path="/pasta" Component={HomePasta} />
+            <Route exact path="/login" Component={Login} />
+            <Route exact path="/registro" Component={Register} />
+            <Route exact path="/termos" Component={Termos} /> */}
+            {/* <Route exact path="*" Component={Pg404} /> */}
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
