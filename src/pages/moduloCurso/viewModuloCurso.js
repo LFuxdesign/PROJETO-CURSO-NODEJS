a302da9df0cc119d9e20625e21cb58b99accefd0
LFuxdesign <leonardofelipe.lf173@gmail.com>
2025-03-04 16:54:47 -0400

    criação parcial da pagina de conteudo do site, e ajustes no json para seguir a logica parecida com modulo1 > sessao 1.1, modulo 2>sessao 2.1

--- src/pages/moduloCurso/viewModuloCurso.js
+++ src/pages/moduloCurso/viewModuloCurso.js
@@ -4,14 +4,15 @@ import Curso from "../../conteudo/curso.json"
 
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import ReactPlayer from "react-player/lazy";
 
 
 export default function ViewModuloCurso() {
     const [searchParams] = useSearchParams(); // parametros url
 
     const [idModulo, setIdModulo] = useState(() => {
-        const urlId = parseInt(searchParams.get("modulo"));
-        return isValidIndex(urlId) && urlId >= 1 ? urlId : 1; // Validando se o índice é válido, caso contrário, ajusta para 1
+        const urlModuleId = parseInt(searchParams.get("modulo"));
+        return isValidIndex(urlModuleId) && urlModuleId >= 1 ? urlModuleId : 1; // Validando se o índice é válido, caso contrário, ajusta para 1
     });
 
     const [viewedModules, setViewedModules] = useState(() => {
@@ -22,7 +23,12 @@ export default function ViewModuloCurso() {
             return parsedModules;
         }
 
-        return Array(Curso.modulos.length).fill({ viewed: false, timestamp: null });
+        return Curso.modulos.map(modulo => ({
+            sessoes: modulo.conteudos.sessoes.map(() => ({
+                viewed: false,
+                timestamp: null,
+            }))
+        }));
     });
 
     const [moduloAtual, setModuloAtual] = useState(Curso.modulos[idModulo - 1]);
@@ -31,53 +37,103 @@ export default function ViewModuloCurso() {
         return index >= 1 && index <= Curso.modulos.length;
     }
 
-    function markAsViewed(index) {
+    function markAsViewed(moduloIndex, sessaoIndex) {
         const newViewedModules = [...viewedModules];
         const timestamp = Date.now();
 
-        if (!newViewedModules[index].viewed) {
-            newViewedModules[index] = { viewed: true, timestamp };
+        if (!newViewedModules[moduloIndex].sessoes[sessaoIndex].viewed) {
+
+            newViewedModules[moduloIndex].sessoes[sessaoIndex] = {
+                viewed: true,
+                timestamp: timestamp,
+            };
+
             setViewedModules(newViewedModules);
             localStorage.setItem("viewedModules", JSON.stringify(newViewedModules));
         }
     }
 
     useEffect(() => {
+        const urlSectionId = parseInt(searchParams.get("sessao")) >= 1 ? parseInt(searchParams.get("sessao")) - 1 : 0;
         setModuloAtual(Curso.modulos[idModulo - 1]);
-        markAsViewed(idModulo - 1);
+        markAsViewed(idModulo - 1, urlSectionId);
         // eslint-disable-next-line
     }, [idModulo]);
 
     useEffect(() => {
-        const newId = parseInt(searchParams.get("modulo"));
-        const validId = isValidIndex(newId) ? newId : 1;
+        const newUrlModuleId = parseInt(searchParams.get("modulo"));
+        const validId = isValidIndex(newUrlModuleId) ? newUrlModuleId : 1;
         setIdModulo(validId);
     }, [searchParams]);
 
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            document.addEventListener("DOMContentLoaded", function () {
+                // Seleciona todos os elementos <video> na página
+                const videos = document.querySelectorAll("video");
+
+                videos.forEach((video) => {
+                    // Remove o atributo "controlsList" para evitar que o usuário baixe o vídeo
+                    video.setAttribute("controlsList", "nodownload");
+
+                    // Bloqueia o botão direito apenas no vídeo
+                    video.addEventListener("contextmenu", (e) => e.preventDefault());
+
+                    // Intercepta e remove atalhos de desenvolvedor
+                    window.addEventListener("keydown", (e) => {
+                        if (
+                            e.ctrlKey &&
+                            (e.key === "s" || e.key === "S" || e.key === "u" || e.key === "U" || e.key === "i" || e.key === "I" || e.key === "j" || e.key === "J")
+                        ) {
+                            e.preventDefault();
+                        }
+                    });
+                });
+            });
+        }, 500);
+    }, [])
+
+
+    function getContrastColor(hexaBackgroundColor, whiteTarget = "rgba(255,255,255,.85)") { //ajustar para contraste de no minimo 4,5:1
+        hexaBackgroundColor = hexaBackgroundColor.replace("#", "");
+        let r = parseInt(hexaBackgroundColor.substring(0, 2), 16);
+        let g = parseInt(hexaBackgroundColor.substring(2, 4), 16);
+        let b = parseInt(hexaBackgroundColor.substring(4, 6), 16);
+
+        let brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+        return brightness > 128 ? "#000" : whiteTarget; // Se for claro, retorna preto, se for escuro, retorna branco
+    }
+
+
     return (
-        <div className="content4moodle moduloCurso">
+        <div className="content4moodle moduloCurso flex flexColumn">
             <section className="header">
                 <div id="title">
                     <h1 dangerouslySetInnerHTML={{ __html: Curso.tituloCurso }} />
                 </div>
             </section>
-            <section className="content">
-                <div className="runningCourses">
+            <section className="content flex">
+                <div className="runningCourses flex flexColumn">
+                    <span style={{ fontSize: "22px" }}>{moduloAtual.titulo}</span>
                     {
-                        Curso.modulos.map((modulo, index) => {
+                        moduloAtual.conteudos.sessoes.map((sessao, index) => {
+                            const sessaoAtual = moduloAtual.conteudos.sessoes[index];
                             return (
-                                <div key={index} className="cardCurso" style={{ background: modulo.colors.highlight }}>
+                                <div key={index} className="cardCurso flexCenter" style={{ background: sessaoAtual.highlightColor }}>
                                     <div className="details">
-                                        <h1 className="titulo">{modulo.titulo}</h1>
-                                        <span className="subTitulo">{modulo.descricao}</span>
+                                        <h1 className="titulo" style={{ color: getContrastColor(sessaoAtual.highlightColor, "#fff") }}>{sessao.titulo}</h1>
+                                        <span className="subTitulo" style={{ color: getContrastColor(sessaoAtual.highlightColor) }}>{sessao.descricao}</span>
                                     </div>
-                                    <div className="info">
-                                        <span>{viewedModules[index].viewed ? "iniciado dia" : "não iniciado"}</span>
-                                        <div className="dia">
+                                    <div className="info flex flexColumn" style={{ gap: "10px" }}>
+                                        <p style={{ color: getContrastColor(sessaoAtual.highlightColor) }}>{viewedModules[idModulo - 1].sessoes[index].viewed ? "iniciado dia" : "não iniciado"}</p>
+                                        <div className="dia flexCenter">
                                             {
                                                 (() => {
-                                                    if (viewedModules[index].timestamp) {
-                                                        const viewedDate = new Date(viewedModules[index].timestamp);
+                                                    if (viewedModules[idModulo - 1].sessoes[index].timestamp) {
+                                                        const viewedDate = new Date(viewedModules[idModulo - 1].sessoes[index].timestamp);
                                                         const viewedDay = viewedDate.getDate().toString().padStart(2, '0');
                                                         return (
                                                             <h1>
@@ -91,15 +147,15 @@ export default function ViewModuloCurso() {
                                         </div>
                                         {
                                             (() => {
-                                                if (viewedModules[index].timestamp) {
-                                                    const viewedDate = new Date(viewedModules[index].timestamp);
+                                                if (viewedModules[idModulo - 1].sessoes[index].timestamp) {
+                                                    const viewedDate = new Date(viewedModules[idModulo - 1].sessoes[index].timestamp);
                                                     const viewedDay = viewedDate.getDate().toString().padStart(2, '0');
                                                     const viewedMonth = (viewedDate.getMonth() + 1).toString().padStart(2, '0');
                                                     const viewedYear = viewedDate.getFullYear().toString().slice(-2);
                                                     return (
-                                                        <span>
+                                                        <p style={{ color: getContrastColor(sessaoAtual.highlightColor) }}>
                                                             {viewedDay + "/" + viewedMonth + "/" + viewedYear}
-                                                        </span>
+                                                        </p>
                                                     );
                                                 }
                                             })()
@@ -109,6 +165,30 @@ export default function ViewModuloCurso() {
                             )
                         })
                     }
+                </div>
+                <div className="conteudoSessao flexCenter flexColumn">
+                    {(() => {
+                        if (moduloAtual.conteudos.sessoes[0].pathVideoSessao) {
+                            return (
+                                <ReactPlayer
+                                    id="videoSessao"
+                                    url={moduloAtual.conteudos.sessoes[0].pathVideoSessao}
+                                    controls={true}
+                                    volume={1}
+                                    config={{
+                                        file: {
+                                            attributes: {
+                                                controlsList: 'nodownload',
+                                                onContextMenu: e => e.preventDefault()
+                                            }
+                                        }
+                                    }}
+                                />
+                            )
+                        }
+                    })()
+                    }
+
                 </div>
             </section>
             {/* <div className="cursesList">
