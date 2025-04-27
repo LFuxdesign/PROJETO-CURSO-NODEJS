fdab31437ff5a2517c58db5057cc78cffcb7ab94
LFuxdesign <leonardofelipe.lf173@gmail.com>
2025-04-27 15:03:34 -0400

    melhorias e correções gerais, melhorias no gerenciamento de cache, melhorias de responsividade

--- src/scripts/getGithubProfileData.js
+++ src/scripts/getGithubProfileData.js
@@ -1,15 +1,37 @@
 export default async function GetGithubProfileData({ username }) {
+  if (!username) return null;
   const url = `https://api.github.com/users/${username}`
   try {
     const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
 
+    if (!response.ok) {
+      let message = "Unknown error";
+      try {
+        const json = await response.json();
+        message = json.message || message;
+      } catch (_) {
+        // Ignore JSON parsing error
+      }
+      const error = new Error("Error getting profile data from GitHub API");
+      error.responseStatus = response.status;
+      error.text = message;
+
+      throw error;
+    }
     const json = await response.json();
-    return json ;
+
+    return json;
   } catch (error) {
-    console.error("Erro ao obter dados de perfil do Github: ", {errorMessage: error.message, Stack: error.stack})
-    return {error: error.message, completeError: error}
+    console.error({
+      error: error.message,
+      responseStatus: error.responseStatus,
+      message: error.text
+    })
+    return {
+      error: true,
+      responseStatus: error.responseStatus,
+      message: error.message,
+      text: error.text
+    };
   }
 }
