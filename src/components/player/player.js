b9bb033dcd7cc534504e59ce8504e7df9407dd1a
LFuxdesign <leonardofelipe.lf173@gmail.com>
2025-04-26 21:53:33 -0400

    melhorias no carregamento de videos com aplicação de skeleton loading

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,59 +1,69 @@
 import ReactPlayerLazy from "react-player/lazy";
 import ReactPlayer from "react-player";
-
-const PlayerIcon = () => {
-    return (
-        <div className="flexCenter playerIcon" title="Reproduzir vídeo">
-            <svg width="249" height="284" viewBox="0 0 249 284" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M241.5 129.01C251.5 134.783 251.5 149.217 241.5 154.99L22.5 281.43C12.5 287.204 1.67928e-05 279.987 1.72975e-05 268.44L2.83512e-05 15.5603C2.8856e-05 4.01331 12.5 -3.2036 22.5 2.5699L241.5 129.01Z" fill="url(#paint0_linear_330_346)" />
-                <defs>
-                    <linearGradient id="paint0_linear_330_346" x1="1295.8" y1="-86.2971" x2="1176.65" y2="559.455" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#2C65A3" />
-                        <stop offset="1" stopColor="#6E67DF" />
-                    </linearGradient>
-                </defs>
-            </svg>
-
-        </div>
-    )
-}
+import { useEffect, useRef, useState } from "react";
 
 export default function Player({ videoPath, animationDelay = 0, loadingType = "lazy" }) {
+    const refSkeleton = useRef(null);
+    const [delay, setDelay] = useState(true)
+    const [showSkeleton, setShowSkeleton] = useState(true);
+
+    useEffect(() => {
+        const delayToLoad = setTimeout(() => {
+            setDelay(false)
+        }, 500);
+        const timer = setTimeout(() => {
+            refSkeleton.current?.classList.add("hideSkeleton");
+            secondTimer = setTimeout(() => {
+                setShowSkeleton(false);
+            }, 550);
+        }, 1700);
+        let secondTimer;
+
+
+        return () => {
+            clearTimeout(delayToLoad);
+            clearTimeout(timer);
+            clearTimeout(secondTimer);
+        };
+    }, []);
+
     return (
-        videoPath && (loadingType === "lazy" ? <ReactPlayerLazy
-            className="videoPlayer"
-            style={{ animationDelay, animationDuration: "1s", marginBottom: "20px" }}
-            url={videoPath}
-            width={"100%"}
-            controls={true}
-            volume={1}
-            light={<img className="thumb" src='/assets/webp/THUMB.webp' alt='' />}
-            playIcon={<PlayerIcon />}
-            config={{
-                file: {
-                    attributes: {
-                        controlsList: 'nodownload',
-                        onContextMenu: e => e.preventDefault()
+        videoPath && (loadingType === "lazy" ? <>
+            {showSkeleton ? <div ref={refSkeleton} className="skeleton" style={{ position: "absolute" }} /> : false}
+            <ReactPlayerLazy
+                className="videoPlayer"
+                style={{ animationDelay, animationDuration: "1s", marginBottom: "20px" }}
+                url={videoPath}
+                width={"100%"}
+                controls={true}
+                volume={1}
+                light={delay ? <div /> : false}
+                config={{
+                    file: {
+                        attributes: {
+                            controlsList: 'nodownload',
+                            onContextMenu: e => e.preventDefault()
+                        }
                     }
-                }
-            }}
-        /> : loadingType === "default" && <ReactPlayer
-            className="videoPlayer"
-            style={{ animationDelay, animationDuration: "1s", marginBottom: "20px" }}
-            url={videoPath}
-            width={"100%"}
-            controls={true}
-            volume={1}
-            light={<img className="thumb" src='/assets/webp/THUMB.webp' alt='' />}
-            playIcon={<PlayerIcon />}
-            config={{
-                file: {
-                    attributes: {
-                        controlsList: 'nodownload',
-                        onContextMenu: e => e.preventDefault()
-                    }
-                }
-            }}
-        />)
+                }}
+            /></> : loadingType === "default" && <>
+                {showSkeleton ? <div ref={refSkeleton} className="skeleton" style={{ position: "absolute" }} /> : false}
+                <ReactPlayer
+                    className="videoPlayer"
+                    style={{ animationDelay, animationDuration: "1s", marginBottom: "20px" }}
+                    url={videoPath}
+                    width={"100%"}
+                    controls={true}
+                    volume={1}
+                    light={delay ? <div /> : false}
+                    config={{
+                        file: {
+                            attributes: {
+                                controlsList: 'nodownload',
+                                onContextMenu: e => e.preventDefault()
+                            }
+                        }
+                    }}
+                /></>)
     )
 }
